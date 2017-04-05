c0b5d5b4bf0b665d9d2c3892e53550c3682e1e24
chenmeng <observerchan@gmail.com>
2017-04-05 22:58:46 +0800

    .

---
+++ wpi/user_sample/config_.py
@@ -0,0 +1,12 @@
+# Warning: Do not save data to this file when this file name is 'config_sample.py' and it under your home directory.
+#          Cause it can be rewritten by program automatic.
+
+
+# Make sure this folder have file '7z.exe' & '7z.dll'
+# z7_dir = r'C:\Program Files (x86)\7-Zip'
+
+
+# drivers_dir = r'Z:\printer_drivers'
+
+
+# archive_exts = ['.zip', '.7z', '.rar', '.exe']
---
+++ wpi2exe/config_.py
@@ -0,0 +1,14 @@
+# Warning: Do not save data to this file when this file name is 'config_sample.py' and it under your home directory.
+#          Cause it can be rewritten by program automatic.
+
+
+# verpathc_path = r'Z:\verpatch.exe'
+
+
+# upx_dir = r'Z:\\'
+
+
+# output_dir = r'Z:\wpi'
+
+
+# output_filename = 'wpi'
